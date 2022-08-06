9a1532227c98e5375993dbacd74a829c0eb8d2e5
Aakash Ghosh <aakash.duke.2019@gmail.com>
2022-08-06 18:42:17 +0530

    abcd

---
+++ src/logger/logger.js
@@ -0,0 +1,4 @@
+let Welcome =function(){
+    console.log("Welcome to my application. I am AAKASH and a part of FunctionUp Plutonium cohort", Welcome)
+}
+module.exports.welMs=Welcome
---
+++ src/validator/formatter.js
@@ -0,0 +1,10 @@
+function formatter(){
+    let a= "                 FUNCTION UP                       "
+    let b= "I Am AaKaSH GhOsH"
+    let c= "aakash ghosh"
+    console.log(a.trim())
+    console.log(b.toLowerCase())
+    console.log(c.toUpperCase())
+}
+
+    module.exports.xyz=formatter
