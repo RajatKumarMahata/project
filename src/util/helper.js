5ee5e3f18f0801a99b78765ee86b60e4aabb7db9
rajatkumarmahata7@gmail.com <you@example.com>
2022-09-03 01:13:52 +0530

    ok

--- src/util/helper.js
+++ src/util/helper.js
@@ -5,12 +5,12 @@ const printDate = function() {
 
 const printMonth = function() {
     let currentdate = new Date()
-    let currentMonth = currentdate.getMonth() + 1
+    let currentMonth = currentdate.getMonth() +1
     console.log('The current month is '+currentMonth)
 }
 
 const getBatchInfo = function() {
-  let batchInformation = 'Radon, W3D4, the topic for today is Nodejs module system assignment discussion'
+  let batchInformation = 'plutonium, W3D4, the topic for today is Nodejs module system assignment discussion'
   console.log(batchInformation)
 }
 
